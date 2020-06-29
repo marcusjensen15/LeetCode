fbd36d8005abcc62f10800967312ca97bfd107ad
Marcus Jensen <marcusjensen1515@gmail.com>
2020-06-29 14:14:41 -0700

    solved candy problem

---
+++ leetcode.js
@@ -0,0 +1,16 @@
+// Correct answers: (Extra candies)
+
+
+// var kidsWithCandies = function(candies, extraCandies) {
+//     let fun = Math.max(...candies);
+//     let output = [];
+//
+//     candies.forEach(function(e){
+//                     var temp = e + extraCandies;
+//                     if (temp < fun) {output.push(false);}
+//                     else {output.push(true)};
+//     });
+//
+//
+//     return output;
+// };
