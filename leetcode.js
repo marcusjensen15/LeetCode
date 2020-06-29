0c39ab96a09e853f3a4d7d937018639aedce0de9
Marcus Jensen <marcusjensen1515@gmail.com>
2020-06-29 14:57:29 -0700

    solved another problem

--- leetcode.js
+++ leetcode.js
@@ -32,3 +32,20 @@
 //     return output;
 //
 // };
+
+
+var runningSum = function(nums) {
+
+    let output = [];
+    let counter = 0;
+
+    for(i=0;i<nums.length;i++){
+        if (nums[i-1] === "undefined"){
+            counter = nums[i];
+        }
+        else {counter = counter + nums[i]}
+
+        output.push(counter);
+    }
+    return output;
+};
